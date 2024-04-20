06b61abcbbb7ab1af5bcf5c0172b8e510f9ace61
main <rjgc2810@gmail.com>
2024-04-20 14:21:38 -0300

    Dns consul

---
+++ tgs-service/docker/entry-point.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+export LOCAL_IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | awk -F':' '{print $2}')
+# generate random id
+UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+export UUID=$LOCAL_IP
+envsubst < service.consul.hcl.tpl > /etc/consul.d/service.consul.hcl
+consul agent -config-dir=/etc/consul.d/ > /tmp/consul-server.log 2>&1 &
+
+iptables -t nat -A PREROUTING -p udp -m udp --dport 53 -j REDIRECT --to-ports 8600
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 8600
+iptables -t nat -A OUTPUT -d localhost -p udp -m udp --dport 53 -j REDIRECT --to-ports 8600
+iptables -t nat -A OUTPUT -d localhost -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 8600
+
+./app
--- tgs-service/service.consul.hcl.tpl
+++ tgs-service/service.consul.hcl.tpl
@@ -2,7 +2,6 @@ service = {
   name = "tgs-service"
   id = "tgs-service-${UUID}"
   port = 8080
-  connect = { sidecar_service = {} }
   check = {
     id = "tgs-check"
     http = "http://${LOCAL_IP}:8080/health",
